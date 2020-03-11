4811a45ed08b7efefe37f6a6325db596a2b9e079
Elias Pinheiro Juk <eliasp.juk@hotmail.com>
2020-03-11 09:33:06 -0300

    Testando estruturas de repetiçao

--- index.js
+++ index.js
@@ -7,29 +7,29 @@ const app = express()
 
 //ROUTES
     app.get('/',(req,res)=>{
-        var nome = "Elias Juk"
-        var lang = "JavaScript"
-        res.render('index',{
-            nome: nome,
-            lang: lang,
-            pais: 'Brasil',
-            estado: 'PR',
-            empresa: '',
-            ano: 2020
-        })
+        res.send("DIGITE /NOME na URL")
     })
 
     app.get('/:nome',(req,res)=>{
-        var nome = req.params.nome
+        let nome = req.params.nome
         //var nome = "Elias Juk"
-        var lang = "JavaScript"
+        let lang = "JavaScript"
+        let exibirMsg = true;
+        
+        var produtos = [
+            {nome: "Doritos", preco: 3.50},
+            {nome: "Coca-cola", preco: 3.80},
+            {nome: "Leite", preco: 1.50}
+        ]
         res.render('index',{
             nome: nome,
             lang: lang,
             pais: 'Brasil',
             estado: 'PR',
             empresa: '',
-            ano: 2020
+            ano: 2020,
+            msg: exibirMsg,
+            produtos: produtos
         })
     })
 
