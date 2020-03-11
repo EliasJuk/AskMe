8af04a50536e7a9fe06e621e39c3fac33800fd43
Elias Pinheiro Juk <eliasp.juk@hotmail.com>
2020-03-11 10:17:13 -0300

    Configurando bootstrap

--- index.js
+++ index.js
@@ -8,33 +8,7 @@ const app = express()
 
 //ROUTES
     app.get('/',(req,res)=>{
-        res.send("DIGITE /NOME na URL")
-    })
-
-    app.get('/:nome',(req,res)=>{
-        let nome = req.params.nome
-        //var nome = "Elias Juk"
-        let lang = "JavaScript"
-        let exibirMsg = true;
-        
-        var produtos = [
-            {nome: "Doritos", preco: 3.50},
-            {nome: "Coca-cola", preco: 3.80},
-            {nome: "Leite", preco: 1.50},
-            {nome: "Carne", preco: 6.00},
-            {nome: "Achocolatado", preco: 2.50},
-            {nome: "Café", preco: 4.50},
-        ]
-        res.render('index',{
-            nome: nome,
-            lang: lang,
-            pais: 'Brasil',
-            estado: 'PR',
-            empresa: '',
-            ano: 2020,
-            msg: exibirMsg,
-            produtos: produtos
-        })
+        res.render('index')
     })
 
 //CONFIG
